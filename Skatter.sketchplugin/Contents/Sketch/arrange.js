7c75bf029a1dbc685f8db6b444cf238d529a5ea2
joshdjuric <joshdjuric@gmail.com>
2017-05-10 14:59:25 +0100

    Work in progress

--- Skatter.sketchplugin/Contents/Sketch/arrange.js
+++ Skatter.sketchplugin/Contents/Sketch/arrange.js
@@ -1,64 +1,107 @@
 //
 // This is no way near finished
 //
-// todo: determine artboard of selected layers
-//    use artboard width and height as basis for area to Skatterify
-//    duplicate selected layers at grid points based on the largest dimenion of the largest shape in the artboard area
+// todo:
 //    figure out cocoa so a dialog to manage randomness and entropy can be developed
 
 @import 'shared.js'
 
 var _w = 1600
 var _h = 640
-var maxDimension = 0;
-
-// var artboard = null
+var maxDimension = 0
+var entropy = 50
+var spaceScale = 1.5
+var selection = []
+var artboards = []
+var artboard = null
 var layers = []
+var container = null
 
 function onRun(context) {
-  var doc = context.document;
-  var selection = context.selection;
+  var sketch = context.api()
+  var doc = context.document
 
-  if (selection.count() == 0) {
-    doc.showMessage('Hey stoops! You need to select some layers to arrange.');
+  selection = sketch.selectedDocument.selectedLayers
+  layers = selection.nativeLayers
+
+  if (layers.count() == 0) {
+    doc.showMessage('Hey stoops! You need to select some layers to arrange.')
     return;
   }
 
-  for (var i = 0; i < selection.count(); i++) {
-    layers.push(selection[i]);
-    maxDimension = Math.max(maxDimension, Math.max(selection[i].frame().width(), selection[i].frame().height()))
+  selection.iterate(function(layer){
+    if (layer.container.isArtboard) {
+      container = layer.container
+    }
+  })
+
+  for (var i = 0; i < layers.count(); i++) {
+    maxDimension = Math.max(maxDimension, Math.max(layers[i].frame().width(), layers[i].frame().height()))
+    parent = layers
   }
 
-  // if (!artboard) {
-  //   doc.showMessage('Hey stoops! You need to select an artboard to arrange on.');
-  //   return;
-  // }
+  maxDimension *= spaceScale
 
-  doc.showMessage('maxDimension: ' + maxDimension)
+  // log('maxDimension: ' + maxDimension)
 
   grid()
+
+  selection.iterate(function(layer){
+    layer.remove()
+  })
 }
 
 function grid() {
-  var cols = Math.floor(_w / maxDimension);
-  var rows = Math.floor(_h / maxDimension);
+  var group = null
+  var prevLayerName = ''
+
+  if (container != null) {
+    _w = container.sketchObject.frame().width();
+    _h = container.sketchObject.frame().height();
+
+    group = container.newGroup();
+  }
+
+  var cols = Math.floor(_w / maxDimension)
+  var rows = Math.floor(_h / maxDimension)
+
+  log('cols: ' + cols + ', rows: ' + rows)
+
   var rowCount = 0
   var colCount = 0
   var units = cols * rows
+  var item = getRandomLayer();
 
   for (var i = 0; i < units; i++) {
-    var item = layers[Math.floor(Math.random() * layers.length)]
+
+    while(item.name() == prevLayerName) {
+      item = getRandomLayer();
+    }
+
+    prevLayerName = item.name()
+
     var dupe = item.duplicate();
-    var posX = colCount * maxDimension
-    var poxY = rowCount * maxDimension
+
+    var posX = colCount * maxDimension + Math.floor(Math.random() * (Math.round(Math.random())?-entropy:entropy))
+    var posY = rowCount * maxDimension + Math.floor(Math.random() * (Math.round(Math.random())?-entropy:entropy))
+
+    // log('posX: ' + posX + ', posY: ' + posY)
 
     if (colCount == cols - 1) {
       colCount = 0
       rowCount++;
     }
+    else {
+      colCount++
+    }
 
-    dupe.frame().setX(posX);
-    dupe.frame().setY(poxY);
-    // randomRotate(dupe);
+    dupe.frame().setX(posX + maxDimension / 2)
+    dupe.frame().setY(posY + maxDimension / 2)
+
+    randomRotate(dupe)
   }
+}
+
+function getRandomLayer() {
+  return layers[Math.floor(Math.random() * layers.length)]
 }
--- Skatter.sketchplugin/Contents/Sketch/shared.js
+++ Skatter.sketchplugin/Contents/Sketch/shared.js
@@ -1,3 +1,9 @@
 function randomRotate(layer) {
   layer.setRotation(Math.random() * 360);
 }
+
+function removeItems(_items) {
+    for (var i = 0; i < _items.count(); i++) {
+      _items[i].remove();
+    }
+}
